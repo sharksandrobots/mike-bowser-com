fe39e42144ef87f5202773a623195029ac3729ea
Mike Bowser <mbowser@gmail.com>
2023-11-18 15:20:33 -0500

    Site update v1

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,19 @@
 module.exports = function(eleventyConfig) {
-    // Return your Object options:
+  // Return your Object options:
 
-    eleventyConfig.addPassthroughCopy("./src/css/");
-    eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addWatchTarget("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addWatchTarget("./src/img");
 
-    return {
-      dir: {
-        input: "src",
-        output: "public"
-      }
-    }
-  };
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    },
+    templateFormats: ['md', 'njk', 'html'],
+    markdownTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+  }
+};
